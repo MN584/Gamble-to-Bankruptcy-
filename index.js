596b27a43427e0a3f230a48286aa778bed7d1679
MN584 <maximilian.neumeie28@student.tamdistrict.org>
2024-10-24 14:56:02 -0700

    if added

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+let spinnerOneEl = document.getElementById("slotValueOne")
+let spinnerTwoEl = document.getElementById("slotValueTwo")
+let spinnerThreeEl = document.getElementById("slotValueThree")
 let valueOne = 0
 let valueTwo = 0
 let valueThree = 0
@@ -11,8 +14,20 @@ function spin() {
   slotValueOne.innerText = valueOne
   slotValueTwo.innerText = valueTwo
   slotValueThree.innerText = valueThree
-  if (valueOne = 7) {
-    
+  if (valueOne == 7) {
+    spinnerOneEl.classList.add("seven")
+  } else {
+    spinnerOneEl.classList.remove("seven")
+  }
+  if (valueTwo == 7) {
+    spinnerTwoEl.classList.add("seven")
+  } else {
+    spinnerTwoEl.classList.remove("seven")
+  }
+  if (valueThree == 7) {
+    spinnerThreeEl.classList.add("seven")
+  } else {
+    spinnerThreeEl.classList.remove("seven")
   }
 }
 
